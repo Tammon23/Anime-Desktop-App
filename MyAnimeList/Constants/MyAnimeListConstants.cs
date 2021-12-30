447fe53ff55ce7c75151949f1a8561809646ad1a
Tammon23 <Tammon2000@gmail.com>
2021-12-30 00:08:21 -0500

    Added Constants

---
+++ MyAnimeList/Constants/MyAnimeListConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace MyAnimeList.Constants
+{
+    public class MyAnimeListConstants
+    {
+        public static string RedirectUrl = "http://localhost:50123/";
+        public static string APIUrl = "https://myanimelist.net/";
+        public static string AuthUrl = "{0}v1/oauth2/authorize?response_type=code&client_id={1}&code_challenge={2}&code_challenge_method=plain";
+        public static string ClientId = "23c477235d19c5349899187d13f5af36";
+
+    }
+}
