d29f2b1688a84847a16ce0d327926459dee8df99
Tammon23 <Tammon2000@gmail.com>
2021-12-30 00:08:37 -0500

    Added Exceptions

---
+++ MyAnimeList/Exceptions/AuthenticationUrlIncomplete.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyAnimeList.Exceptions
+{
+    [Serializable]
+    public class AuthenticationUrlIncomplete : Exception
+    {
+        public AuthenticationUrlIncomplete()
+        {
+        }
+
+        public AuthenticationUrlIncomplete(string message) 
+            : base(message)
+        {
+        }
+
+        public AuthenticationUrlIncomplete(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+    }
+}
---
+++ MyAnimeList/Exceptions/HttpClientNotInitialized.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyAnimeList.Exceptions
+{
+    [Serializable]
+    public class HttpClientNotInitialized : Exception
+    {
+        public HttpClientNotInitialized()
+        {
+        }
+
+        public HttpClientNotInitialized(string message) 
+            : base(message)
+        {
+        }
+
+        public HttpClientNotInitialized(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+    }
+}
