c0f62a1d69370d26c3e69be9625338caeae7132c
Tammon23 <Tammon2000@gmail.com>
2022-01-01 00:36:14 -0500

    Updated Get url method to send proper headers

--- MyAnimeList/MALRequestClient.cs
+++ MyAnimeList/MALRequestClient.cs
@@ -1,48 +1,57 @@
 ﻿using System;
-using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+
 
 namespace MyAnimeList
 {
     public class MALRequestClient
     {
         private static HttpClient _myAnimeListClient;
-        public const string CLIENT_ID = "23c477235d19c5349899187d13f5af36";
         
         /// <summary>
         /// A static constructor used to create the httpclient
         /// </summary>
-        static void Init()
+        public static void Init()
         {
-            OAuth.Init();
-            OAuth.Login();
+            //OAuth.Init();
+            //OAuth.Login();
             
             if (_myAnimeListClient == null)
             {
                 _myAnimeListClient = HttpClientFactory.Create();
                 _myAnimeListClient.DefaultRequestHeaders.Accept.Clear();
-                _myAnimeListClient.BaseAddress = new Uri("https://api.myanimelist.net");
+                _myAnimeListClient.BaseAddress = new Uri("https://api.myanimelist.net/");
                 _myAnimeListClient.DefaultRequestHeaders.Accept.Add(
                     new MediaTypeWithQualityHeaderValue("application/json"));
             }
         }
+        
         /// <summary>
         /// Used to preform a get request to my anime list
         /// </summary>
         /// <param name="url"> The url that a request will be made to </param>
+        /// <param name="requiresLogin"> Whether the request needs the user to be logged in to be fulfilled </param>
         /// <typeparam name="T"> Generic return type </typeparam>
         /// <returns> A formatted object of type T, null if bad request  </returns>
-        public static async Task<T> Get<T>(string url)
+        public static async Task<T?> Get<T>(string url, bool requiresLogin = false)
         {
-            var response = await _myAnimeListClient.GetAsync(url);
+            using var requestMessage = new HttpRequestMessage(HttpMethod.Get, url);
+            if (requiresLogin)
+                requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", "token goes here");
+            
+            else
+                requestMessage.Headers.Add("X-MAL-CLIENT-ID", Constants.MyAnimeListConstants.ClientId);
+            
+            /*Root myDeserializedClass = JsonSerializer.Deserialize<Root>(myJsonResponse)*/
+            var response = await _myAnimeListClient.SendAsync(requestMessage);
             if (response.StatusCode == HttpStatusCode.OK)
             {
                 return await response.Content.ReadAsAsync<T>();
             }
-
+            
             return default;
         }
         
