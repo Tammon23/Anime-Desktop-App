80c7c7854a1d708b32e8b545388bb8685e8209d6
Tammon23 <Tammon2000@gmail.com>
2022-01-01 00:33:52 -0500

    Changed static variables to const variables

--- MyAnimeList/Constants/MyAnimeListConstants.cs
+++ MyAnimeList/Constants/MyAnimeListConstants.cs
@@ -2,10 +2,10 @@
 {
     public class MyAnimeListConstants
     {
-        public static string RedirectUrl = "http://localhost:50123/";
-        public static string APIUrl = "https://myanimelist.net/";
-        public static string AuthUrl = "{0}v1/oauth2/authorize?response_type=code&client_id={1}&code_challenge={2}&code_challenge_method=plain";
-        public static string ClientId = "23c477235d19c5349899187d13f5af36";
+        public const string RedirectUrl = "http://localhost:50123/";
+        public const string APIUrl = "https://myanimelist.net/";
+        public const string AuthUrl = "{0}v1/oauth2/authorize?response_type=code&client_id={1}&code_challenge={2}&code_challenge_method=plain";
+        public const string ClientId = "23c477235d19c5349899187d13f5af36";
 
     }
 }
