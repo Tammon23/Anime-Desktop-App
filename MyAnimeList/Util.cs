73571b53f6724edddf1629904a4248c05ed54da8
Tammon23 <Tammon2000@gmail.com>
2022-01-02 00:08:46 -0500

    Added utility functions for enum to string logic

---
+++ MyAnimeList/Util.cs
@@ -0,0 +1,30 @@
+﻿using MyAnimeList.ResponseObjects.Anime;
+
+namespace MyAnimeList
+{
+    public class Util
+    {
+        public static string RankingTypeToString(RankingTypeEnum rankingType) => rankingType switch
+        {
+            RankingTypeEnum.Airing => "airing",   
+            RankingTypeEnum.All => "all",   
+            RankingTypeEnum.Favorite => "favorite",   
+            RankingTypeEnum.Movie => "movie",   
+            RankingTypeEnum.Ova => "ova",   
+            RankingTypeEnum.Special => "special",   
+            RankingTypeEnum.Tv => "tv",
+            RankingTypeEnum.Upcoming => "upcoming",   
+            RankingTypeEnum.ByPopularity => "bypopularity",
+            _ => "all"
+        };
+        
+        public static string SeasonToString(SeasonEnum season) => season switch
+        {
+            SeasonEnum.Winter => "winter",
+            SeasonEnum.Spring => "spring",
+            SeasonEnum.Summer => "summer",
+            SeasonEnum.Fall => "fall",
+            _ => "winter"
+        };
+    }
+}
