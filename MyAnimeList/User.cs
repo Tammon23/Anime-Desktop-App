d4c1431d04179dcc6e432ab98f33f6c8030fc459
Tammon23 <Tammon2000@gmail.com>
2022-01-03 00:20:07 -0500

    Added User Class

---
+++ MyAnimeList/User.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using MyAnimeList.ResponseObjects.User;
+
+namespace MyAnimeList
+{
+    public class User
+    {
+        public User()
+        {
+        }
+        
+        public async Task<AnimeListStatus?> UpdateAnimeListStatus(int animeId, FormUrlEncodedContent contentToUpdate)
+        {
+            var r = await MALRequestClient.Patch<AnimeListStatus>(
+                $"v2/anime/{animeId}/my_list_status", contentToUpdate);
+            return r;
+        }
+    }
+}
