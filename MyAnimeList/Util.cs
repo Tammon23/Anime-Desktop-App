fbb899c60d4426ab383ef499331d9749e2613e88
Tammon23 <Tammon2000@gmail.com>
2022-01-03 00:19:45 -0500

    Added StatusToString function, reformatted code

--- MyAnimeList/Util.cs
+++ MyAnimeList/Util.cs
@@ -4,27 +4,37 @@ namespace MyAnimeList
 {
     public class Util
     {
-        public static string RankingTypeToString(RankingTypeEnum rankingType) => rankingType switch
+        public static string RankingTypeToString(RankingTypeEnum? rankingType) => rankingType switch
         {
-            RankingTypeEnum.Airing => "airing",   
-            RankingTypeEnum.All => "all",   
-            RankingTypeEnum.Favorite => "favorite",   
-            RankingTypeEnum.Movie => "movie",   
-            RankingTypeEnum.Ova => "ova",   
-            RankingTypeEnum.Special => "special",   
-            RankingTypeEnum.Tv => "tv",
-            RankingTypeEnum.Upcoming => "upcoming",   
+            RankingTypeEnum.Airing       => "airing",
+            RankingTypeEnum.All          => "all",
+            RankingTypeEnum.Favorite     => "favorite",
+            RankingTypeEnum.Movie        => "movie",
+            RankingTypeEnum.Ova          => "ova",
+            RankingTypeEnum.Special      => "special",
+            RankingTypeEnum.Tv           => "tv",
+            RankingTypeEnum.Upcoming     => "upcoming",
             RankingTypeEnum.ByPopularity => "bypopularity",
-            _ => "all"
+            _                            => "all"
         };
-        
-        public static string SeasonToString(SeasonEnum season) => season switch
+
+        public static string SeasonToString(SeasonEnum? season) => season switch
         {
             SeasonEnum.Winter => "winter",
             SeasonEnum.Spring => "spring",
             SeasonEnum.Summer => "summer",
-            SeasonEnum.Fall => "fall",
-            _ => "winter"
+            SeasonEnum.Fall   => "fall",
+            _                 => "winter"
+        };
+
+        public static string StatusToString(StatusEnum? status) => status switch
+        {
+            StatusEnum.Completed   => "completed",
+            StatusEnum.Dropped     => "dropped",
+            StatusEnum.Watching    => "watching",
+            StatusEnum.OnHold      => "on_hold",
+            StatusEnum.PlanToWatch => "plan_to_watch",
+            _                      => "plan_to_watch"
         };
     }
 }
