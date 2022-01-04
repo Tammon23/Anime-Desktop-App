ff24785419c1bfb471e504986d2ec9725d104328
Tammon23 <Tammon2000@gmail.com>
2022-01-04 00:15:08 -0500

    Updated User.cs - Added handling for getting a users anime list - Added handling for deleting an item from a users anime list

--- MyAnimeList/User.cs
+++ MyAnimeList/User.cs
@@ -1,6 +1,10 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using System.Threading.Tasks;
+using MyAnimeList.Exceptions;
+using MyAnimeList.ResponseObjects.Anime;
 using MyAnimeList.ResponseObjects.User;
+using MyAnimeList.ResponseObjects.User.MyAnimeList.ResponseObjects.User;
 
 namespace MyAnimeList
 {
@@ -14,6 +18,50 @@ namespace MyAnimeList
         {
             var r = await MALRequestClient.Patch<AnimeListStatus>(
                 $"v2/anime/{animeId}/my_list_status", contentToUpdate);
+            return r;
+        }
+        
+        /// <summary>
+        /// Gets the user's anime list
+        /// </summary>
+        /// <param name="username">Username of the anime list, defaults to @me</param>
+        /// <param name="status">Filters returned anime list by these statuses. To return all anime, don't specify this field (null)</param>
+        /// <param name="sort">The method to stor by (anime id under development)</param>
+        /// <param name="offset">The amount of results to skip over when retrieving results</param>
+        /// <param name="limit">The max amount of results</param>
+        /// <returns>AnimeListStatus object or null if failure</returns>
+        /// <exception cref="LimitOutOfRangeException">Limit must be between 0 and 1000 inclusively</exception>
+        
+        public async Task<UserAnimeList?> GetUserAnimeList(string username = "@me",
+            StatusEnum? status = null,
+            SortEnum? sort = null,
+            int offset = 0,
+            int limit = 4)
+        {
+            if (limit is > 1000 or < 0)
+            {
+                throw new LimitOutOfRangeException("Limit must be between 0 and 1000");
+            }
+
+            var r = await MALRequestClient.Get<UserAnimeList>(
+                $"v2/users/{username}/animelist?fields=list_status&offset={offset}&limit={limit}" 
+                    + $"{(status == null ? "" : "&status=" + Util.StatusToString(status))}"
+                    + $"{(sort == null ? "" : "&sort=" + Util.SortToString(sort))}",
+                username == "@me"
+            );
+            
+            return r;
+        }
+        
+        /// <summary>
+        /// Removes an anime from the users anime list 
+        /// </summary>
+        /// <param name="animeId">The ID of the anime to be removed</param>
+        /// <returns>True if the delete was successful, False if the anime ID is invalid, null remaining cases</returns>
+        public async Task<bool?> DeleteAnimeListItem(int animeId)
+        {
+            var r = await MALRequestClient.Delete(
+                $"v2/anime/{animeId}/my_list_status");
             return r;
         }
     }
