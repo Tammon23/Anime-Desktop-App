2616aff115cdf14554f313b4a1bdfcf969025d74
Tammon23 <Tammon2000@gmail.com>
2022-01-05 00:20:13 -0500

    Added new response object

---
+++ MyAnimeList/ResponseObjects/User/UserInformation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace MyAnimeList.ResponseObjects.User
+{
+    [DataContract]
+    public class UserInformation : User
+    {
+        public UserInformation(int id, string name, string location, DateTime joinedAt, AnimeStatistics animeStatistics)
+            : base(id, name)
+        {
+            this.Location = location;
+            this.JoinedAt = joinedAt;
+            this.AnimeStatistics = animeStatistics;
+        }
+        
+        [DataMember]
+        public string Location { get; }
+
+        [DataMember(Name="joined_at")]
+        public DateTime JoinedAt { get; }
+
+        [DataMember(Name="anime_statistics")]
+        public AnimeStatistics AnimeStatistics { get; }
+
+        public override string ToString()
+        {
+            return base.ToString()
+                   + $"Location: {Location}, "
+                   + $"Joined At: {JoinedAt}, "
+                   + $"Anime Statistics: {AnimeStatistics} stats is null {AnimeStatistics == null}"
+                ;
+        }
+    }
+}
