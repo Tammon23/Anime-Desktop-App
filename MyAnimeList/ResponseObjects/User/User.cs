6a6c7066c0cc6dd5e572714945411af80e292b72
Tammon23 <Tammon2000@gmail.com>
2022-01-05 00:19:59 -0500

    Added a base class for user the all new user types will inherit from

---
+++ MyAnimeList/ResponseObjects/User/User.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.Serialization;
+
+namespace MyAnimeList.ResponseObjects.User
+{
+    [DataContract]
+    public class User
+    {
+        public User(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+
+        [DataMember]
+        public int Id { get; }
+
+        [DataMember]
+        public string Name { get; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, "
+                    + $"Name: {Name}"
+                ;
+        }
+    }
+
+}
