05dac26e97ab1e69a48b6a82b764b5a8a94ca852
Tammon23 <Tammon2000@gmail.com>
2022-01-06 23:40:35 -0500

    Datum -> ForumTopicDatum

--- MyAnimeList/ResponseObjects/Forum/ForumTopicDetail.cs
+++ MyAnimeList/ResponseObjects/Forum/ForumTopicDetail.cs
@@ -1,27 +1,28 @@
-﻿using System.Runtime.Serialization;
-using MyAnimeList.ResponseObjects.Anime;
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using MyAnimeList.ResponseObjects.General;
 
 namespace MyAnimeList.ResponseObjects.Forum
 {
     [DataContract]
     public class ForumTopicDetail
     {
-        public ForumTopicDetail(Data data, Paging paging)
+        public ForumTopicDetail(List<ForumTopicDatum> data, Paging paging)
         {
             this.Data = data;
             this.Paging = paging;
         }
 
         [DataMember]
-        public Data Data { get; }
+        public IReadOnlyList<ForumTopicDatum> Data { get; }
 
         [DataMember]
         public Paging Paging { get; }
 
         public override string ToString()
         {
-            return $"Data: {Data}, "
-                    + $"Paging: {Paging}"
+            return $"Data: {(Data != null ? string.Join(" | ", Data) : "N/A")}, "
+                   + $"Paging: {Paging}"
                 ;
         }
     }
