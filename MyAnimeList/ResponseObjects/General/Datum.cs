97266be0f9b40fbb9886f9d74cea75e4bbf97af7
Tammon23 <Tammon2000@gmail.com>
2022-01-06 23:37:39 -0500

    Added base general classes

---
+++ MyAnimeList/ResponseObjects/General/Datum.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+
+namespace MyAnimeList.ResponseObjects.General
+{
+    public class Datum
+    {
+        public Datum(Node node)
+        {
+            this.Node = node;
+        }
+
+        [DataMember]
+        public Node Node { get; }
+
+        public override string ToString()
+        {
+            return $"Node: {Node}";
+        }
+    }
+}
---
+++ MyAnimeList/ResponseObjects/General/Root.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using MyAnimeList.ResponseObjects.Anime;
+
+namespace MyAnimeList.ResponseObjects.General
+{
+    [DataContract]
+    public class Root<T>
+    {
+        public Root(List<T> data, Paging paging)
+        {
+            this.Data = data;
+            this.Paging = paging;
+        }
+
+        [DataMember]
+        public IReadOnlyList<T> Data { get; }
+
+        [DataMember]
+        public Paging Paging { get; }
+        
+        public override string ToString()
+        {
+            return $"Data: {(Data != null ? string.Join(" | ", Data) : "N/A")}, "
+                   + $"Paging: {Paging}"
+                ;
+        }
+    }
+}
