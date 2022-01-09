1d54d70c097e1cd519a488459a1887a5c21f163a
Tammon23 <Tammon2000@gmail.com>
2022-01-08 20:49:54 -0500

    Added no response objects

---
+++ MyAnimeList/ResponseObjects/Manga/Serialization.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.Serialization;
+using MyAnimeList.ResponseObjects.General;
+
+namespace MyAnimeList.ResponseObjects.Manga
+{   
+    [DataContract]
+    public class Serialization
+    {
+        
+        public Serialization(Node node, string role)
+        {
+            this.Node = node;
+            this.Role = role;
+        }
+
+        [DataMember]
+        public Node Node { get; }
+
+        [DataMember]
+        public string Role { get; }
+        
+        public override string ToString()
+        {
+            return $"Node: {Node}"
+                    + $"Role: {Role} "
+                ;
+        }
+    }
+}
