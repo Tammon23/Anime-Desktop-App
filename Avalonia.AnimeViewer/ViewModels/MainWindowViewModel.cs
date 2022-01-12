51cfeca18f60561b86c17df7c3ac8da0e6eadff0
Tammon23 <Tammon2000@gmail.com>
2022-01-11 22:36:09 -0500

    Added new ViewModel - ViewModel asynchronously gets an image from a URL then converts into a bitmap usable by the view

---
+++ Avalonia.AnimeViewer/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using ReactiveUI;
+
+namespace Avalonia.AnimeViewer.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private string chessboardUrl = "https://cdn.myanimelist.net/images/anime/1988/119437.jpg";
+        public string ChessboardUrl
+        {
+            get => chessboardUrl;
+            set {
+                this.RaiseAndSetIfChanged(ref chessboardUrl, value);
+                DownloadImage(ChessboardUrl);
+                System.Diagnostics.Debug.WriteLine(ChessboardUrl);
+            }
+        }
+
+        private Avalonia.Media.Imaging.Bitmap chessboard = null;
+        public Avalonia.Media.Imaging.Bitmap Chessboard
+        {
+            get => chessboard;
+            set => this.RaiseAndSetIfChanged(ref chessboard, value);
+        }
+
+        public MainWindowViewModel()
+        {
+            DownloadImage(ChessboardUrl);
+        }
+
+        public void DownloadImage(string url)
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.DownloadDataAsync(new Uri(url));
+                client.DownloadDataCompleted += DownloadComplete;
+            }
+        }
+
+        private void DownloadComplete(object sender, DownloadDataCompletedEventArgs e)
+        {
+            try
+            {
+                byte[] bytes = e.Result;
+
+                Stream stream = new MemoryStream(bytes);
+
+                var image = new Avalonia.Media.Imaging.Bitmap(stream);
+                Chessboard = image;
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex);
+                Chessboard = null; // Could not download...
+            }
+            
+        }
+    }
+}
