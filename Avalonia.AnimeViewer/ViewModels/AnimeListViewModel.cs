f41b943c3e5ebffbe21946c3463ef83de5125664
Tammon23 <Tammon2000@gmail.com>
2022-01-11 22:24:24 -0500

    Added a test ViewModel

---
+++ Avalonia.AnimeViewer/ViewModels/AnimeListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using MyAnimeList.ResponseObjects.Anime;
+
+namespace Avalonia.AnimeViewer.ViewModels
+{
+    public class AnimeListViewModel : ViewModelBase
+    {
+        public AnimeListViewModel(IEnumerable<Datum> items)
+        {
+            Items = new ObservableCollection<Datum>(items);
+        }
+        
+        public ObservableCollection<Datum> Items { get; }
+    }
+}
