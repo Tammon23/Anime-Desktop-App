c01ed85a8a5cc74504ad10a0d9fe81f4f0eee88d
Tammon23 <Tammon2000@gmail.com>
2022-01-16 23:33:32 -0500

    Updated MainWindowViewModel.cs
    - Removed unused properties

--- Avalonia.AnimeViewer/ViewModels/MainWindowViewModel.cs
+++ Avalonia.AnimeViewer/ViewModels/MainWindowViewModel.cs
@@ -10,10 +10,8 @@ namespace Avalonia.AnimeViewer.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        
         private string? _animeSearchText;
         private bool _isBusy;
-        private string _imageLink = "https://cdn.myanimelist.net/images/anime/1988/119437.jpg";
         private CancellationTokenSource? _cancellationTokenSource;
 
         public MainWindowViewModel()
@@ -36,28 +34,19 @@ namespace Avalonia.AnimeViewer.ViewModels
             get => _isBusy;
             set => this.RaiseAndSetIfChanged(ref _isBusy, value);
         }
-
-        public string AnimeTitle
-        {
-            get => _imageLink;
-            set {
-                this.RaiseAndSetIfChanged(ref _imageLink, value);
-                System.Diagnostics.Debug.WriteLine(AnimeTitle);
-            }
-        }
-
+        
         public ObservableCollection<AnimeSearchViewModel> SearchForAnimeResult { get; } = new();
 
         private async void DoAnimeSearch(string searchTerm)
         {
             IsBusy = true;
             SearchForAnimeResult.Clear();
-            
+
             _cancellationTokenSource?.Cancel();
             _cancellationTokenSource = new CancellationTokenSource();
 
             var animeSearchResults = await AnimeSearch.SearchAsync(searchTerm);
-
+            
             foreach (var anime in animeSearchResults)
             {
                 var vm = new AnimeSearchViewModel(anime);
@@ -83,13 +72,6 @@ namespace Avalonia.AnimeViewer.ViewModels
                     return;
                 }
             }
-        }
-
-        private Avalonia.Media.Imaging.Bitmap _imageBitmap = null;
-        public Avalonia.Media.Imaging.Bitmap ImageBitmap
-        {
-            get => _imageBitmap;
-            set => this.RaiseAndSetIfChanged(ref _imageBitmap, value);
         }
     }
 }
