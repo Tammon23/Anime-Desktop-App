22839a863adc6243b1d7c898ee1c99bca917047b
Tammon23 <Tammon2000@gmail.com>
2022-01-17 22:29:26 -0500

    Transitioning to a inheritance style

---
+++ MyAnimeList/ResponseObjects/General/NSFWEnum.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace MyAnimeList.ResponseObjects.General;
+
+[DataContract, JsonConverter(typeof(StringEnumConverter))]
+public enum NsfwEnum
+{
+    [EnumMember(Value = "white")] [Description("This work is safe for work")]
+    White,
+    
+    [EnumMember(Value = "gray")] [Description("This work may be not safe for work")]
+    Gray,
+    
+    [EnumMember(Value = "black")] [Description("This work is not safe for work")]
+    Black
+}
