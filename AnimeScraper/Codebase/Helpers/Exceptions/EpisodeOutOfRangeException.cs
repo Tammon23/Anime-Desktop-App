228311eab6a3e67e31027eb4437bc30d5473c17d
Tammon23 <Tammon2000@gmail.com>
2022-06-20 21:14:01 -0400

    Introduced new exceptions for better end user debugging

---
+++ AnimeScraper/Codebase/Helpers/Exceptions/EpisodeOutOfRangeException.cs
@@ -0,0 +1,16 @@
+﻿namespace AnimeScraper.Codebase.Helpers.Exceptions;
+
+public class EpisodeOutOfRangeException : Exception
+{
+    public EpisodeOutOfRangeException()
+    {
+    }
+
+    public EpisodeOutOfRangeException(string message) : base(message)
+    {
+    }
+
+    public EpisodeOutOfRangeException(string message, Exception inner) : base(message, inner)
+    {
+    }
+}
---
+++ AnimeScraper/Codebase/Helpers/Exceptions/InvalidRangeException.cs
@@ -0,0 +1,16 @@
+﻿namespace AnimeScraper.Codebase.Helpers.Exceptions;
+
+public class InvalidRangeException : Exception
+{
+    public InvalidRangeException()
+    {
+    }
+
+    public InvalidRangeException(string message) : base(message)
+    {
+    }
+
+    public InvalidRangeException(string message, Exception inner) : base(message, inner)
+    {
+    }
+}
