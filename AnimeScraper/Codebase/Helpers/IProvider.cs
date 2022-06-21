7767174eb95570ef0346a3d858e7a0f0e6d335c9
Tammon23 <Tammon2000@gmail.com>
2022-06-20 21:13:34 -0400

    Removed methods that will no longer be implemented
    - Future versions will instead use GrabAnimeEpisodeRange(), GrabAnimeEpisode(), and their overloads. If all goes to plan, this will be part of ISearcher

--- AnimeScraper/Codebase/Helpers/IProvider.cs
+++ AnimeScraper/Codebase/Helpers/IProvider.cs
@@ -2,13 +2,5 @@
 
 public interface IProvider : ISearcher
 {
-    public void GetStream(string uri);
-    public Task GetHtml();
     public ProviderEnum GetProviderType();
-
-    /// <summary>
-    /// Static method used to get the instance of the provider, as providers follow the singleton design pattern
-    /// </summary>
-    /// <returns></returns>
-    /*public IProvider GetProvider();*/
 }
