49ca9390b010f6ca6d3bb8f252e88d1f0407b07c
Tammon23 <Tammon2000@gmail.com>
2023-03-15 22:53:03 -0400

    Improved Anime Details Page - Added more data about the anime in a more organized (still unfinished) mannor

--- GUI/ViewModels/AnimeDetailsViewModel.cs
+++ GUI/ViewModels/AnimeDetailsViewModel.cs
@@ -1,10 +1,14 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Avalonia.Media.Imaging;
+using GUI.Models;
 using MyAnimeList;
 using MyAnimeList.FieldManager;
 using MyAnimeList.ResponseObjects.Anime;
+using MyAnimeList.ResponseObjects.General;
 using ReactiveUI;
 
 namespace GUI.ViewModels;
@@ -12,6 +16,16 @@ namespace GUI.ViewModels;
 public class AnimeDetailsViewModel : ReactiveObject, IRoutableViewModel
 {
     private static bool _clientInitialized;
+    private IReadOnlyCollection<Node> AnimeRecommendations { get; init; }
+    private IReadOnlyCollection<Node> AnimeRelated { get; init; }
+    private IReadOnlyCollection<Node> MangaRelated { get; init; }
+    
+    public AnimeDetails? Details { get; private init; } // add ^ in view if does not work as is
+
+    public string UrlPathSegment => "Details_Page";
+    public IScreen HostScreen { get; }
+
+    public string? Art => Details?.MainPicture.Large ?? Details?.MainPicture.Medium;
 
     private AnimeDetailsViewModel(IScreen screen)
     {
@@ -25,19 +39,41 @@ public class AnimeDetailsViewModel : ReactiveObject, IRoutableViewModel
 
     public static async Task<AnimeDetailsViewModel> CreateAsync(IScreen screen, int id)
     {
+        var details = await GetDetails(id);
+        List<Node> animeRecommendations = new();
+        List<Node> relatedAnime = new();
+        List<Node> relatedManga = new();
+        
+        if (details != null)
+        {
+            animeRecommendations.AddRange(details.Recommendations.Select(recAnime => new NodePresentation(recAnime.Node, screen)
+            {
+                UseLargePicture = true // eventually will be migrated to the settings plane
+            }));
+            
+            relatedAnime.AddRange(details.RelatedAnime.Select(relAnime => new NodePresentation(relAnime.Node, screen)
+            {
+                UseLargePicture = true
+            }));
+            
+            relatedManga.AddRange(details.RelatedManga.Select(relManga => new NodePresentation(relManga.Node, screen)
+            {
+                UseLargePicture = true
+            }));
+        }
+
         var result = new AnimeDetailsViewModel(screen)
         {
-            Details = await GetDetails(id)
+            Details = details,
+            AnimeRecommendations = animeRecommendations,
+            AnimeRelated = relatedAnime,
+            MangaRelated = relatedManga
         };
+
         return result;
     }
     
-    public AnimeDetails? Details { get; private init; } // add ^ in view if does not work as is
 
-    public string UrlPathSegment => "Details_Page";
-    public IScreen HostScreen { get; }
-
-    public string? Art => Details?.MainPicture.Large ?? Details?.MainPicture.Medium;
     
     public static async Task<AnimeDetails?> GetDetails(int id)
     {
--- GUI/Views/AnimeDetailsView.axaml.cs
+++ GUI/Views/AnimeDetailsView.axaml.cs
@@ -1,4 +1,5 @@
-﻿using Avalonia.Markup.Xaml;
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
 using Avalonia.ReactiveUI;
 using GUI.ViewModels;
 using ReactiveUI;
@@ -7,9 +8,20 @@ namespace GUI.Views;
 
 public class AnimeDetailsView : ReactiveUserControl<AnimeDetailsViewModel>
 {
+
+    private Button _left;
+    private Button _right;
+    private Carousel _carousel;
     public AnimeDetailsView()
     {
         InitializeComponent();
+
+        _carousel = this.Get<Carousel>("Carousel");
+        _left = this.Get<Button>("Left");
+        _right = this.Get<Button>("Right");
+        
+        _left.Click += (_, _) => _carousel.Previous();
+        _right.Click += (_, _) => _carousel.Next();
     }
 
     private void InitializeComponent()
