e69b31b1914a369d930650f4ab11161c04263bec
Tammon23 <Tammon2000@gmail.com>
2023-03-18 15:39:55 -0400

    Added Recommendations and Seasonal anime pages

--- GUI/ViewModels/RecommendationsPageViewModel.cs
+++ GUI/ViewModels/RecommendationsPageViewModel.cs
@@ -1,13 +1,20 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using GUI.Models;
+using MyAnimeList.ResponseObjects.General;
 using ReactiveUI;
 
 namespace GUI.ViewModels;
 
 public class RecommendationsPageViewModel : ReactiveObject, IRoutableViewModel
 {
+    public ObservableCollection<Node> QueriedAnimeRecommendations { get; } = new();
+
     public RecommendationsPageViewModel(IScreen screen)
     {
         HostScreen = screen;
+        GetRecommended();
     }
 
     // Reference to IScreen that owns the routable view model
@@ -15,4 +22,22 @@ public class RecommendationsPageViewModel : ReactiveObject, IRoutableViewModel
 
     // Unique identifier for the routable view model
     public string? UrlPathSegment { get; } = Guid.NewGuid().ToString().Substring(0, 5);
+    
+    public async Task GetRecommended()
+    {
+        QueriedAnimeRecommendations.Clear();
+
+        // searching for the anime using the provided search term
+        var animeRecommendations = await AnimeRecommendations.GetRecommendedAsync();
+
+        foreach (var anime in animeRecommendations)
+        {
+            var node = new NodePresentation(anime.ContentNode, HostScreen)
+            {
+                UseLargePicture = true // eventually will be migrated to the settings plane
+            };
+            QueriedAnimeRecommendations.Add(node);
+        }
+    }
+
 }
--- GUI/ViewModels/SeasonalAnimePageViewModel.cs
+++ GUI/ViewModels/SeasonalAnimePageViewModel.cs
@@ -1,13 +1,63 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading;
+using System.Threading.Tasks;
+using GUI.Models;
+using MyAnimeList.ResponseObjects.Anime;
+using MyAnimeList.ResponseObjects.General;
 using ReactiveUI;
+using AnimeSeasonal = GUI.Models.AnimeSeasonal;
 
 namespace GUI.ViewModels;
 
 public class SeasonalAnimePageViewModel : ReactiveObject, IRoutableViewModel
 {
+    private int _year;
+    private int _selectedSeason;
+    
+    public ObservableCollection<Node> QueriedAnimeSeasonal { get; } = new();
+    public int MaximumYear { get; set; }
+    
+    /*
+    public int Year { get; set; }
+    public int SelectedSeason { get; set; }
+    */
+    
+    public int Year
+    {
+        get => _year;
+        set => this.RaiseAndSetIfChanged(ref _year, value);
+    }
+
+    public int SelectedSeason
+    {
+        get => _selectedSeason; 
+        set => this.RaiseAndSetIfChanged(ref _selectedSeason, value);
+    }
+    
     public SeasonalAnimePageViewModel(IScreen screen)
     {
         HostScreen = screen;
+        var time = DateTime.Now;
+
+        Year = time.Year;
+        MaximumYear = Year;
+        SelectedSeason = time.Month switch
+        {
+            <= 3 => 0,
+            > 3 and <= 6 => 1,
+            > 6 and <= 9 => 2,
+            _ => 3
+        };
+        
+        this.WhenAnyValue(x => x.Year, x => x.SelectedSeason)
+            .Throttle(TimeSpan.FromSeconds(1), RxApp.MainThreadScheduler)
+            .SelectMany(GetSeasonalAnime)
+            .Subscribe();
     }
 
     // Reference to IScreen that owns the routable view model
@@ -15,4 +65,31 @@ public class SeasonalAnimePageViewModel : ReactiveObject, IRoutableViewModel
 
     // Unique identifier for the routable view model
     public string? UrlPathSegment { get; } = Guid.NewGuid().ToString().Substring(0, 5);
+
+    private async Task<Unit> GetSeasonalAnime((int, int) _, CancellationToken cancel)
+    {
+        QueriedAnimeSeasonal.Clear();
+        
+        var selectedSeason = SelectedSeason switch
+        {
+            0 => SeasonEnum.Winter,
+            1 => SeasonEnum.Spring,
+            2 => SeasonEnum.Summer,
+            _ => SeasonEnum.Fall
+        };
+        
+        // searching for the anime using the provided search term
+        var animeSeasonal = await AnimeSeasonal.GetSeasonalAnime(Year, selectedSeason);
+
+        foreach (var anime in animeSeasonal)
+        {
+            var node = new NodePresentation(anime.ContentNode, HostScreen)
+            {
+                UseLargePicture = true // eventually will be migrated to the settings plane
+            };
+            QueriedAnimeSeasonal.Add(node);
+        }
+        
+        return Unit.Default;
+    }
 }
