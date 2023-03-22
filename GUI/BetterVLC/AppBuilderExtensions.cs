abeaa6141cb7fc81d68f0d583695b72d2c3663fa
Tammon23 <Tammon2000@gmail.com>
2023-03-22 17:41:46 -0400

    added manifest

---
+++ GUI/BetterVLC/AppBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Controls;
+using LibVLCSharp.Shared;
+
+namespace GUI.BetterVLC;
+
+public static class AppBuilderExtensions
+{
+    public static T UseVLCSharp<T>(this AppBuilderBase<T> b, LibVLCAvaloniaRenderingOptions? renderingOptions = null, string libvlcDirectoryPath = null)
+        where T : AppBuilderBase<T>, new()
+    {
+        if (renderingOptions != null)
+        {
+            LibVLCAvaloniaOptions.RenderingOptions = renderingOptions.Value;
+        }
+
+        return b.AfterSetup(_ => Core.Initialize(libvlcDirectoryPath));
+    }
+}
---
+++ GUI/BetterVLC/LibVLCAvaloniaOptions.cs
@@ -0,0 +1,23 @@
+﻿namespace GUI.BetterVLC;
+public enum LibVLCAvaloniaRenderingOptions
+{
+    /// <summary>
+    /// uses native handle passed to vlc, best possible performance
+    /// </summary>
+    VlcNative,
+
+    /// <summary>
+    /// uses default avalonia rendering with image
+    /// </summary>
+    Avalonia,
+
+    /// <summary>
+    /// uses default avalonia rendering with custom drawing operation, expected better performace with avalonia
+    /// </summary>
+    AvaloniaCustomDrawingOperation
+}
+
+internal class LibVLCAvaloniaOptions
+{
+    public static LibVLCAvaloniaRenderingOptions RenderingOptions { get; set; } = LibVLCAvaloniaRenderingOptions.VlcNative;
+}
