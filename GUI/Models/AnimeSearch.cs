979ce139af900e150189890c05df5d223e5236d3
Tammon23 <Tammon2000@gmail.com>
2023-03-31 23:17:01 -0400

    cleaned up imports

--- GUI/Models/AnimeSearch.cs
+++ GUI/Models/AnimeSearch.cs
@@ -1,10 +1,6 @@
 ﻿using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Net.Http;
-using System.Text.Json;
 using System.Threading.Tasks;
-using Microsoft.CodeAnalysis.CSharp;
 using MyAnimeList;
 using MyAnimeList.ResponseObjects.General;
 
