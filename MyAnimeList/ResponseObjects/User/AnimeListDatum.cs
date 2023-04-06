28c2306edcc31ac3319b970022ea28a5589454ab
Tammon23 <Tammon2000@gmail.com>
2023-04-05 22:22:05 -0400

    Fixed bug relating to improper inheritance

--- MyAnimeList/ResponseObjects/User/AnimeListDatum.cs
+++ MyAnimeList/ResponseObjects/User/AnimeListDatum.cs
@@ -1,18 +1,20 @@
-﻿using System.Runtime.Serialization;
+﻿using System.Net;
+using System.Runtime.Serialization;
 using MyAnimeList.ResponseObjects.Anime;
+using MyAnimeList.ResponseObjects.General;
 
 namespace MyAnimeList.ResponseObjects.User;
 
 [DataContract]
-public class AnimeListDatum
+public class AnimeListDatum : AnimeDatum
 {
-    public AnimeListDatum(AnimeNode node, ListStatus listStatus)
+    public AnimeListDatum(AnimeNode node, ListStatus listStatus) : base(node)
     {
         Node = node;
         ListStatus = listStatus;
     }
 
-    [DataMember] public AnimeNode Node { get; }
+    [DataMember] public new AnimeNode Node { get; }
 
     [DataMember(Name = "list_status")] public ListStatus ListStatus { get; }
 
