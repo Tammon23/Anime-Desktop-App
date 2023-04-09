7876c15fecfcc370da0d45876aa49ddd7ea010a4
Tammon23 <Tammon2000@gmail.com>
2023-04-08 21:55:20 -0400

    Updated timetable scraper - Fixed obtaining duplicates of the same anime - Removed the week day groups since timezones make it irrelevant - Removed pretty print

--- get_timetables.py
+++ get_timetables.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timezone
-from typing import Union, Dict
+from typing import Union, Dict, List
 import logging.handlers
 from lxml import etree
 import requests
@@ -14,36 +14,38 @@ def get_timetable_from_web():
     return etree.HTML(r.text)
 
 
-def get_timetables(html) -> Dict:
-    result = {}
-    day_result = []
+def get_timetables(html) -> List[Dict[str, str]]:
+    seen = set()
+    result = []
     show_result = {}
     day_divs = html.xpath("//div[contains(@class, 'timetable-column ')]")
     day_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
     for day in day_divs:
         current_class = day.get("class")
 
-        for show in day.xpath("//div[contains(@class, 'timetable-column-show ')]"):
-            inner = show.xpath(".//h3[@class='time-bar']")[0]
-            language = inner.xpath(".//span")[1].text
-            if language != "SUB" and language != "DUB":
-                continue
-
-            show_result["language"] = language
-
-            show_result["raw_name"] = show.get('route')
-            show_result["clean_name"] = show.xpath(".//a/h2")[0].text
-            show_result["episode"] = re.search(r'\d+', inner.xpath(".//span")[0].text).group()
-            show_result["datetime"] = inner.xpath(".//time")[0].get("datetime")
-
-            day_result.append(show_result.copy())
-            show_result.clear()
-
         for check_day in day_of_week:
             if check_day in current_class:
-                result[check_day] = day_result.copy()
 
-        day_result.clear()
+                for show in day.xpath("//div[contains(@class, 'timetable-column-show ')]"):
+                    inner = show.xpath(".//h3[@class='time-bar']")[0]
+                    language = inner.xpath(".//span")[1].text
+                    if language != "SUB" and language != "DUB":
+                        continue
+
+                    if (show_id := show.get('showid'), language) in seen:
+                        continue
+                    else:
+                        seen.add((show_id, language))
+
+                    show_result["language"] = language
+
+                    show_result["raw_name"] = show.get('route')
+                    show_result["clean_name"] = show.xpath(".//a/h2")[0].text
+                    show_result["episode"] = re.search(r'\d+', inner.xpath(".//span")[0].text).group()
+                    show_result["datetime"] = inner.xpath(".//time")[0].get("datetime")
+
+                    result.append(show_result.copy())
+                    show_result.clear()
 
     return result
 
@@ -58,7 +60,8 @@ def get_mal_id(raw_name: str) -> Union[None, str]:
         return None
 
 
-def save_result(result: Dict, filename: str = "anime_weekly_timetable.json", pretty_print: bool = True) -> None:
+def save_result(result: List[Dict[str, str]], filename: str = "anime_weekly_timetable.json",
+                pretty_print: bool = True) -> None:
     created_at = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S.%f%z")
     result = {"created_at": created_at, "timetable": result}
     with open(filename, "w") as write_file:
@@ -83,17 +86,16 @@ if __name__ == '__main__':
 
     logger.info("Starting action")
     tree = get_timetable_from_web()
-    json_dict = get_timetables(tree)
+    all_shows = get_timetables(tree)
 
     num_anime = 0
 
-    for day in json_dict:
-        for i, show in enumerate(json_dict[day]):
-            num_anime += 1
-            if (mal_id := get_mal_id(show['raw_name'])) is not None:
-                json_dict[day][i]["mal_id"] = mal_id
+    for i, show in enumerate(all_shows):
+        num_anime += 1
+        if (mal_id := get_mal_id(show['raw_name'])) is not None:
+            all_shows[i]["mal_id"] = mal_id
 
     logger.info(f"Processed {num_anime} shows")
 
-    save_result(json_dict)
+    save_result(all_shows, pretty_print=False)
     logger.info("Finished updating json")
