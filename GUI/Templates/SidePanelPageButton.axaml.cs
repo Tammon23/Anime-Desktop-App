39112d6e4ffdc68ef377869c8278d29f54d4e124
Tammon23 <Tammon2000@gmail.com>
2023-04-23 23:26:00 -0400

    Redesign
    - Groupboxes now use a transparent background design for a more modern look
    -Side panel page buttons actually now work as initially intended, using much cleaner code

--- GUI/Templates/SidePanelPageButton.axaml.cs
+++ GUI/Templates/SidePanelPageButton.axaml.cs
@@ -1,17 +1,11 @@
-﻿using System.Diagnostics;
-using System.Windows.Input;
-using Avalonia;
+﻿using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Media;
 
 namespace GUI.Templates;
 
-public class SidePanelPageButton : RadioButton/*TemplatedControl*/
+public class SidePanelPageButton : RadioButton
 {
-
-    public static readonly StyledProperty<string> TextProperty = AvaloniaProperty.Register<SidePanelPageButton, string>(
-        nameof(Text), "Button Text");
-
     public static readonly StyledProperty<int> IconSizeProperty = AvaloniaProperty.Register<SidePanelPageButton, int>(
         nameof(IconSize), 24);
 
@@ -21,42 +15,18 @@ public class SidePanelPageButton : RadioButton/*TemplatedControl*/
 
     public static readonly StyledProperty<Geometry> IconDataProperty =
         AvaloniaProperty.Register<SidePanelPageButton, Geometry>(
-            nameof(IconData),
+            nameof(IconData)/*,
             Geometry.Parse(
-                "M12,12L14.33,16H9.68L12,12M12,8L6.21,18H17.8L12,8M12,2L1,21H23L12,2M12,6L19.53,19H4.47L12,6Z"));
+                "M12,12L14.33,16H9.68L12,12M12,8L6.21,18H17.8L12,8M12,2L1,21H23L12,2M12,6L19.53,19H4.47L12,6Z")*/);
 
-    public static readonly StyledProperty<int> BorderWidthProperty =
-        AvaloniaProperty.Register<SidePanelPageButton, int>(
-            nameof(BorderWidth), 48);
+    public static readonly StyledProperty<Thickness> IconMarginProperty =
+        AvaloniaProperty.Register<SidePanelPageButton, Thickness>(
+            nameof(IconMargin));
 
-    /*
-    public static readonly StyledProperty<ICommand> CommandProperty =
-        AvaloniaProperty.Register<SidePanelPageButton, ICommand>(
-            nameof(Command));*/
-
-    /*public static readonly StyledProperty<string> GroupNameProperty = AvaloniaProperty.Register<SidePanelPageButton, string>(
-        "GroupName");*/
-
-    /*public static readonly StyledProperty<bool> IsCheckedProperty = AvaloniaProperty.Register<SidePanelPageButton, bool>(
-        "IsChecked");*/
-
-    /*public bool IsChecked
-    {
-        get => GetValue(IsCheckedProperty);
-        set => SetValue(IsCheckedProperty, value);
-    }*/
-    /*public string GroupName
-    {
-        get => GetValue(GroupNameProperty);
-        set => SetValue(GroupNameProperty, value);
-    }*/
-
-    public string Text
-    {
-        get => GetValue(TextProperty);
-        set => SetValue(TextProperty, value);
-    }
-
+    public static readonly StyledProperty<bool> ShowTextProperty =
+        AvaloniaProperty.Register<SidePanelPageButton, bool>(
+            nameof(ShowText), true);
+    
     public int IconSize
     {
         get => GetValue(IconSizeProperty);
@@ -75,15 +45,15 @@ public class SidePanelPageButton : RadioButton/*TemplatedControl*/
         set => SetValue(IconDataProperty, value);
     }
 
-    public int BorderWidth
+    public Thickness IconMargin
     {
-        get => GetValue(BorderWidthProperty);
-        set => SetValue(BorderWidthProperty, value);
+        get => GetValue(IconMarginProperty);
+        set => SetValue(IconMarginProperty, value);
     }
 
-    /*public ICommand Command
+    public bool ShowText
     {
-        get => GetValue(CommandProperty);
-        set => SetValue(CommandProperty, value);
-    }*/
+        get => GetValue(ShowTextProperty);
+        set => SetValue(ShowTextProperty, value);
+    }
 }
