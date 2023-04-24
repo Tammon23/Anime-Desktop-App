6278f1c14f80758a1921fbbc8114a82cbdde205b
Tammon23 <Tammon2000@gmail.com>
2023-04-23 23:43:39 -0400

    fixup! Added more verbose types

---
+++ MyAnimeList/ResponseObjects/Anime/AnimeMediaType.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace MyAnimeList.ResponseObjects.Anime;
+
+[DataContract]
+[JsonConverter(typeof(StringEnumConverter))]
+public enum AnimeMediaType
+{ 
+    Unknown,
+    Tv,
+    Ova,
+    Movie,
+    Special,
+    Ona,
+    Music
+}
---
+++ MyAnimeList/ResponseObjects/Anime/AnimeSourceTypeEnum.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace MyAnimeList.ResponseObjects.Anime;
+
+[DataContract]
+[JsonConverter(typeof(StringEnumConverter))]
+public enum AnimeSourceTypeEnum
+{
+    Other,
+    Original,
+    Manga,
+    [EnumMember(Value = "4_koma_manga")]
+    Four_koma_manga,
+    Web_manga,
+    Digital_manga,
+    Novel,
+    Light_novel,
+    Visual_novel,
+    Game,
+    Card_game,
+    Book,
+    Picture_book,
+    Radio,
+    Music
+
+}
